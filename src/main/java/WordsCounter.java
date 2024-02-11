719186d05099868b011bb674f67c5a4b6163c155
Olena Zakharina <strin000@gmail.com>
2024-02-11 13:24:57 +0200

    sorted method added to task 3

--- src/main/java/WordsCounter.java
+++ src/main/java/WordsCounter.java
@@ -1,15 +1,16 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 // task 3
 public class WordsCounter {
     public static void main(String[] args) {
 
         String fileName = "files/words.txt";
-        displayWords(countWords(fileName));
+        Map<String, Integer> unsortedList = countWords(fileName);
+        List<Map.Entry<String, Integer>> sortedEntries = entriesSortedByValues(unsortedList);
+        displayWords(sortedEntries);
 
     }
 
@@ -34,8 +35,25 @@ public class WordsCounter {
         return wordsList;
     }
 
-    public static void displayWords(Map<String, Integer> wordsList) {
-        for (Map.Entry<String, Integer> entry : wordsList.entrySet()) {
+    public static <K,V extends Comparable<? super V>> List<Map.Entry<K, V>> entriesSortedByValues(Map<K,V> map) {
+
+        List<Map.Entry<K,V>> sortedEntries = new ArrayList<>(map.entrySet());
+
+        Collections.sort(sortedEntries,
+                new Comparator<Map.Entry<K,V>>() {
+                    @Override
+                    public int compare(Map.Entry<K,V> e1, Map.Entry<K,V> e2) {
+                        return e2.getValue().compareTo(e1.getValue());
+                    }
+                }
+        );
+
+        return sortedEntries;
+    }
+
+    public static void displayWords(List<Map.Entry<String, Integer>> sortedEntries) {
+              for (Map.Entry<String, Integer> entry : sortedEntries) {
+
             System.out.println(entry.getKey() + " " + entry.getValue());
         }
     }
